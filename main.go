e28cfdad5e88b2c5d9933758b06363719723d89e
wiki0 <wiki0@live.cn>
2017-12-07 22:58:46 +0800

    加入聊天记录

--- main.go
+++ main.go
@@ -9,14 +9,13 @@ import (
 
 var clients = make(map[*websocket.Conn]bool) // connected clients
 var broadcast = make(chan Message)           // broadcast channel
-var historyArrary = [3][2]string{} //聊天历史
+var historyArrary = [3][2]string{}           //聊天历史
 // Configure the upgrader
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
-
 // Define our message object
 type Message struct {
 	Username string `json:"username"`
@@ -25,9 +24,9 @@ type Message struct {
 
 func main() {
 	// Create a simple file server
-	path,_ := os.Getwd()
+	path, _ := os.Getwd()
 	log.Println(path)
-	fs := http.FileServer(http.Dir(path+"/public/"))
+	fs := http.FileServer(http.Dir(path + "/public/"))
 	http.Handle("/", fs)
 
 	// Configure websocket route
@@ -57,30 +56,26 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	clients[ws] = true
 
 	//聊天历史
-	if historyArrary[0][0]==""{
-		historyArrary[0][0]="wiki"
-		historyArrary[0][1]="有什么想说的,这还没有人"
+	if historyArrary[0][0] == "" {
+		historyArrary[0][0] = "wiki"
+		historyArrary[0][1] = "有什么想说的,这还没有人"
+		historyArrary[1][0] = "wiki"
+		historyArrary[1][1] = "信息已加密"
+		historyArrary[2][0] = "wiki"
+		historyArrary[2][1] = "开始吧"
 	}
-	for x:=0;x<3 ; x++ {
-		var history Message
-		if historyArrary[x][0]!="" {
-			history.Username = historyArrary[x][0]
-			history.Message = historyArrary[x][1]
-			err := ws.WriteJSON(history)
-			if err != nil {
-				log.Printf("error: %v", err)
-				ws.Close()
-				delete(clients, ws)
-			}
-		}
+	var history Message
+	for x := 0; x < 3; x++ {
+		history.Username = historyArrary[x][0]
+		history.Message = historyArrary[x][1]
+		ws.WriteJSON(history)
 	}
-
 	for {
 		var msg Message
 		// Read in a new message as JSON and map it to a Message object
 		err := ws.ReadJSON(&msg)
 		if err != nil {
-			log.Printf("error: %v", err)
+			//log.Printf("error: %v", err)
 			delete(clients, ws)
 			break
 		}
@@ -93,33 +88,22 @@ func handleMessages() {
 	for {
 		// Grab the next message from the broadcast channel
 		msg := <-broadcast
+
+		for x := 0; x < 2; x++ {
+			historyArrary[x][0] = historyArrary[x+1][0]
+			historyArrary[x][1] = historyArrary[x+1][1]
+		}
+		historyArrary[2][0] = msg.Username
+		historyArrary[2][1] = msg.Message
+
 		// Send it out to every client that is currently connected
 		for client := range clients {
-			index := 0
-			for x:=0;x<3 ; x++ {
-				if historyArrary[x][0]=="" {
-					index =x
-					break
-				}
-			}
-			if index <3 {
-				historyArrary[index][0]=msg.Username
-				historyArrary[index][1]=msg.Message
-			}else{
-				for x:=0;x<2 ; x++ {
-					historyArrary[x][0]=historyArrary[x+1][0]
-					historyArrary[x][1]=historyArrary[x+1][1]
-				}
-				historyArrary[2][0]=msg.Username
-				historyArrary[2][1]=msg.Message
-			}
 			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Printf("error: %v", err)
+				//log.Printf("error: %v", err)
 				client.Close()
 				delete(clients, client)
 			}
 		}
 	}
 }
-
