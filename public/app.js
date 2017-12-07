b9693726aef7ad60ecae40d126b3475d9953be6e
yangfan <shiki0@qq.com>
2017-12-07 15:37:17 +0800

    解决跨区问题和Anti HotLinking

--- public/app.js
+++ public/app.js
@@ -7,27 +7,90 @@ var vm = new Vue({
         chatContent: '', // A running list of chat messages displayed on the screen
         username: '', // Our username
         message: 'Hello Vue!',
+        images: {},
         joined: false // True if email and username have been filled in
     },
 
-    created: function() {
+    created: function () {
         var self = this;
         this.ws = new WebSocket('ws://' + window.location.host + '/ws');
-        this.ws.addEventListener('message', function(e) {
+        this.ws.addEventListener('message', function (e) {
             var msg = JSON.parse(e.data);
             var name = document.getElementById("name").value;
-            if (msg.username == name){
-                self.chatContent +='<div class="media"><div class="media-left">'
-                    +'<img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + self.gravatarURL(msg.username) + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>'
-                    +'<div class="media-body"><h4 class="media-heading">'
-                    + msg.username
-                    +'</h4>' + emojione.toImage(msg.message) + '</div>';
-            }else{
-                self.chatContent +='<div class="media">'
-                    +'<div class="media-body"><h4 class="media-heading text-right">'
-                    + msg.username
-                    +'</h4><div class="text-right">' + emojione.toImage(msg.message) + '</div></div>'
-                    +'<div class="media-right"><img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + self.gravatarURL(msg.username) + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>';
+
+            if (msg.username in self.images) {
+
+                if (msg.username === name) {
+                    self.chatContent += '<div class="media"><div class="media-left">'
+                        + '<img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + self.images[msg.username] + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>'
+                        + '<div class="media-body"><h4 class="media-heading">'
+                        + msg.username
+                        + '</h4>' + emojione.toImage(msg.message) + '</div>';
+                } else {
+                    self.chatContent += '<div class="media">'
+                        + '<div class="media-body"><h4 class="media-heading text-right">'
+                        + msg.username
+                        + '</h4><div class="text-right">' + emojione.toImage(msg.message) + '</div></div>'
+                        + '<div class="media-right"><img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + self.images[msg.username] + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>';
+                }
+
+            } else {
+                console.log("false");
+                var image = 'http://www.gravatar.com/avatar/';
+                $.ajax({
+                    url: "https://www.googleapis.com/customsearch/v1",
+                    data: {
+                        q: msg.username,
+                        key: 'AIzaSyCqPGFsZjjPgp7cG5KTi3OhYKoMofeLiWI',
+                        cx: '010386887610032054917:nibt9ljmdaa',
+                        imgType: 'photo',
+                        imgSize: 'medium',
+                        searchType: 'image',
+                        num: '3'
+                    },
+                    type: "get",
+                    dataType: 'jsonp',
+                    async: false,
+                    success: function (data) {
+                        var flag = false;
+                        for (var i=0;i<3;i++){
+                            if (!flag){
+                                image = data.items[i].link;
+                                console.log(image);
+                                $.ajax({
+                                    url: image,
+                                    type: 'GET',
+                                    async: false,
+                                    complete: function(response){
+                                        if(response.status != 403){
+                                            flag = true;
+                                        }else{
+                                           console.log('403');
+                                        }
+                                    }
+                                });
+                            }
+                        }
+
+                        if (msg.username === name) {
+                            self.chatContent += '<div class="media"><div class="media-left">'
+                                + '<img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + image + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>'
+                                + '<div class="media-body"><h4 class="media-heading">'
+                                + msg.username
+                                + '</h4>' + emojione.toImage(msg.message) + '</div>';
+                        } else {
+                            self.chatContent += '<div class="media">'
+                                + '<div class="media-body"><h4 class="media-heading text-right">'
+                                + msg.username
+                                + '</h4><div class="text-right">' + emojione.toImage(msg.message) + '</div></div>'
+                                + '<div class="media-right"><img class="media-object" data-src="holder.js/54x54" alt="54x54" src="' + image + '" data-holder-rendered="true" style="width: 54px; height: 54px;"></div>';
+                        }
+                        self.images[msg.username] = image; // map.put(key, value);
+                    },
+                    error: function () {
+                        console.log("error");
+                    }
+                });
             }
 
             var element = document.getElementById('chat-messages');
@@ -37,13 +100,13 @@ var vm = new Vue({
 
     methods: {
         send: function () {
-            if (this.newMsg != '') {
+            if (this.newMsg !== '') {
                 this.ws.send(
                     JSON.stringify({
-                        username: this.username,
-                        message: $('<p>').html(this.newMsg).text() // Strip out html
-                    }
-                ));
+                            username: this.username,
+                            message: $('<p>').html(this.newMsg).text() // Strip out html
+                        }
+                    ));
                 this.newMsg = ''; // Reset newMsg
             }
         },
@@ -51,32 +114,6 @@ var vm = new Vue({
         join: function () {
             this.username = $('<p>').html(this.username).text();
             this.joined = true;
-        },
-
-        gravatarURL: function(username) {
-            var image = 'http://www.gravatar.com/avatar/';
-            $.ajax({
-                url: "https://www.googleapis.com/customsearch/v1",
-                data: {
-                    q: username,
-                    key:'AIzaSyCqPGFsZjjPgp7cG5KTi3OhYKoMofeLiWI',
-                    cx:'010386887610032054917:nibt9ljmdaa',
-                    imgType:'photo',
-                    imgSize:'medium',
-                    searchType:'image',
-                    num:'1'
-                },
-                async: false,
-                type: "get",
-                success: function (data) {
-                    image = data.items[0].link;
-                },
-                error: function(){
-                    console.log("error")
-                }
-            });
-            console.log(image);
-            return image;
         }
     }
 
